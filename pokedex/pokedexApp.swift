837ae08325b2c72ba94a97127b38c0d8ea1deeb7
Kevin Dallian <kevindallian@gmail.com>
2024-05-21 10:51:06 +0700

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Kevin Dallian on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
